9618c9c812e545dc8d8849fbb0e776e4233c8243
Puja Raksa <pujaraksa.id@gmail.com>
2023-02-02 21:02:24 +0800

    fix js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // Change Color
-function ubahLatar(){
-    const color = document.getElementById('inputHexa').value;
-    const card = document.getElementsByClassName('card')[0];
+// function ubahLatar(){
+//     const color = document.getElementById('inputHexa').value;
+//     const card = document.getElementsByClassName('card')[0];
 
-    card.style.backgroundColor=color;
+//     card.style.backgroundColor=color;
+// }
+
+// Change Color
+const targetObject = document.getElementById('targetObject');
+const inputHexa = document.getElementById('inputHexa');
+
+function ubahLatar() {
+    targetObject.style.backgroundColor = inputHexa.value
 }
